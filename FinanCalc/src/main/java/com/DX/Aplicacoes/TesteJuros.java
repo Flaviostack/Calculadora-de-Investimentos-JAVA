ce73087ef43d291fd953a08b0582602d6dca2664
Flávio Augusto N.S.tack <62808034+Flaviostack@users.noreply.github.com>
2025-01-12 10:58:03 -0300

    Update TesteJuros.java

--- FinanCalc/src/main/java/com/DX/Aplicacoes/TesteJuros.java
+++ FinanCalc/src/main/java/com/DX/Aplicacoes/TesteJuros.java
@@ -2,10 +2,12 @@ package com.DX.Aplicacoes;
 
 public class TesteJuros {
     public static void main(String args[]){
-        JurosSimples j1 = new JurosSimples(100.00, 0.01,10);
+        JurosSimples j1 = new JurosSimples();
+        j1.pedirDados();
         j1.calcularJuros();
 
-        JurosCompostos jc1 = new JurosCompostos(100,0.02,10);
+        JurosCompostos jc1 = new JurosCompostos();
+        jc1.pedirDados();
         jc1.calcularJuros();
 
     }
