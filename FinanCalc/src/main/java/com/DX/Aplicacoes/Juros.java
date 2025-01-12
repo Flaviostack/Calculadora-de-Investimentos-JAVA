0687b6be83660dbcede838cc3e68568a6b3c9d12
Flávio Augusto N.S.tack <62808034+Flaviostack@users.noreply.github.com>
2025-01-11 21:15:18 -0300

    Update Juros.java

--- FinanCalc/src/main/java/com/DX/Aplicacoes/Juros.java
+++ FinanCalc/src/main/java/com/DX/Aplicacoes/Juros.java
@@ -5,7 +5,6 @@ package com.DX.Aplicacoes;
     public double juros;
     public double taxa;
     public int tempoDeAplicacao;
-    public double capitalFinal;
 
     public Juros(double capital, double taxa, int tempoDeAplicacao){
         this.capital = capital;
