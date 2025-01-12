905223c9dc850c4d7da616a5694955a592c7e4dc
Flávio Augusto N.S.tack <62808034+Flaviostack@users.noreply.github.com>
2025-01-12 10:59:06 -0300

    Update Juros.java

--- FinanCalc/src/main/java/com/DX/Aplicacoes/Juros.java
+++ FinanCalc/src/main/java/com/DX/Aplicacoes/Juros.java
@@ -1,17 +1,34 @@
 package com.DX.Aplicacoes;
+import java.util.Scanner;
 
- public abstract class Juros {
+public abstract class Juros {
     public double capital;
     public double juros;
     public double taxa;
     public int tempoDeAplicacao;
+    Scanner entrada = new Scanner(System.in);
 
+    //Construtores
     public Juros(double capital, double taxa, int tempoDeAplicacao){
         this.capital = capital;
         this.taxa = taxa;
         this.tempoDeAplicacao = tempoDeAplicacao;
     }
+    public Juros(){}
 
+    //Métodos
     abstract void calcularJuros();
 
+    public void pedirDados(){
+        System.out.print("Digite o valor inicial da aplicação: ");
+        capital = entrada.nextDouble();entrada.nextLine();
+
+        System.out.print("Digite a porcentagem da taxa: %");
+        taxa = entrada.nextDouble();entrada.nextLine();
+        taxa = taxa/100;
+
+        System.out.print("Digite a duração da aplicação em meses: ");
+        tempoDeAplicacao = entrada.nextInt();entrada.nextLine();
+
+    }
 }
